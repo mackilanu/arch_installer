877f490d9e3157732dcb13a501ec6b4174c2fa6a
Marcus Andersson <mackilanu@gmail.com>
2021-03-31 00:03:43 +0200

    First full functional version

---
+++ first_part.py
@@ -0,0 +1,49 @@
+import os
+
+os.system('loadlkeys sv-latin1')
+
+skip_part = input('Skip partitioning? (y/N): ')
+
+if skip_part.lower() != 'y':
+    os.system('fdisk -l')
+    disk = input('Pick a disk to install on: ')
+
+    ans = input('This disk will be wiped, please backup your data before proceeding. Continue? (Y/n)')
+    if ans == 'n':
+        exit()
+
+    ans_swap = input('Do you want a swap partition? (y/N): ')
+
+    os.system('wipefs -a -p ' + disk)
+    os.system(f'parted {disk} --script mklabel gpt')
+    os.system(f'parted {disk} --script mkpart primary fat32 0% 513MiB')
+    ans_swap_size = ""
+    if ans_swap.lower() == 'y':
+        ans_swap_size = input("How big of a swap partition do you want? (in mb)")
+        os.system(f'parted {disk} --script mkpart primary linux-swap 513MiB {ans_swap_size}MiB')
+    os.system(f'parted {disk} --script set 1 esp on')
+    os.system(f'parted {disk} --script mkpart primary ext4 {int(ans_swap_size) + 513}MiB 100%')
+
+    if 'nvme' in disk:
+        disk = disk + 'p'
+
+    ext4_part_num = ""
+    if ans_swap.lower() == 'y':
+        ext4_part_num = '3'
+    else:
+        ext4_part_num = '2'
+
+    os.system(f'mkfs.fat -F 32 {disk}1')
+    os.system(f'mkfs.ext4 {disk}{ext4_part_num}')
+    if ans_swap.lower() == 'y':
+        os.system(f"mkswap {disk}2")
+        os.system(f"swapon {disk}2")
+
+    os.system(f'mount {disk}{ext4_part_num} /mnt')
+    os.system(f'mkdir /mnt/EFI')
+    os.system(f'mount {disk}1 /mnt/EFI')
+
+os.system('pacstrap /mnt base linux linux-firmware base-devel vim git python grub efibootmgr reflector curl')
+os.system('genfstab -U /mnt >> /mnt/etc/fstab')
+os.system('curl -L 172.104.240.145/second.html > /mnt/inst')
+os.system('arch-chroot /mnt')
---
+++ second_part.py
@@ -0,0 +1,58 @@
+import os
+
+os.system('ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime')
+os.system('hwclock --systohc')
+os.system('rm /etc/locale.gen')
+
+os.system("curl -L 172.104.240.145/locale.html > /etc/locale.gen")
+os.system('locale-gen')
+
+hostname = input('Enter your desired hostname: ')
+
+os.system(f"printf '{hostname}' /etc/hostname")
+
+hosts = """127.0.0.1 localhost
+::1 localhost
+127.0.1.1 {0}.localdomain {0}""".format(hostname)
+
+reflector = """--save /etc/pacman.d/mirrorlist
+--protocol https
+--country Sweden,Norway
+--latest 5
+--sort age
+"""
+
+os.system(f"printf \"{reflector}\" > /etc/xdg/reflector/reflector.conf")
+
+os.system(f"printf \"{hosts}\" > /etc/hosts")
+os.system("curl -L 172.104.240.145/sudoers.html > /etc/sudoers")
+
+username = input('Enter desired username: ')
+os.system(f'useradd -mG wheel {username}')
+
+print('Enter password below')
+os.system(f'passwd {username}')
+
+print('Enter root below')
+os.system('passwd')
+
+de = input('Please enter desired DE(gnome/kde): ')
+
+if de == 'gnome':
+    os.system('pacman -S gnome gdm')
+    os.system('systemctl enable gdm')
+if de == 'kde':
+    os.system('pacman -S plasma kde-applications sddm')
+    os.system('systemctl enable sddm')
+
+cpu = input('Do you have an amd or intel CPU? (amd/intel): ')
+
+os.system(f"pacman -S {cpu}-ucode")
+
+os.system('grub-install --target=x86_64-efi --efi-directory=/EFI --bootloader-id=GRUB')
+
+os.system('grub-mkconfig -o /boot/grub/grub.cfg')
+
+os.system('systemctl enable NetworkManager reflector')
+
+print('The installation script has finished! Enjoy your new system :)')
